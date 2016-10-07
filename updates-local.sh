c0969d8757fff5494ff8c66e587644a67de25343
MardonHH <markusmenzel@gmail.com>
2016-10-07 16:42:11 +0200

    Create updates-local.sh

---
+++ updates-local.sh
@@ -0,0 +1,11 @@
+# Kernel: add ZEN i/o sheduler
+cherries+=(LX_1368)
+
+# New Bootlogo: More material like color
+cherries+=(LX_1369)
+
+# Kernel: OC up to 1,8GHz
+cherries+=(LX_1370)
+
+# init: set default interactive max cpu clock @1,4GHz
+cherries+=(LX_1371)
