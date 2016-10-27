f0feebf817ce7df24aafa6ac648ec5975430c864
MardonHH <markusmenzel@gmail.com>
2016-10-27 22:55:30 +0200

    Update updates-local.sh

--- updates-local.sh
+++ updates-local.sh
@@ -1,17 +1,17 @@
 # Kernel: add ZEN i/o sheduler
-cherries+=(LX_1368)
+cherries+=(LX_1470)
 
 # New Bootlogo: More material like color
-cherries+=(LX_1369)
+cherries+=(LX_1471)
 
 # Kernel: OC up to 1,8GHz
-cherries+=(LX_1370)
+cherries+=(LX_1472)
 
 # init: set default interactive max cpu clock @1,4GHz
-cherries+=(LX_1371)
-
-# Use Camera HAL1
-cherries+=(LX_1404)
+cherries+=(LX_1473)
 
 # media_profiles: Enable a working true 720p mode
-cherries+=(LX_1406)
+cherries+=(LX_1474)
+
+# Use Camera HAL1
+# cherries+=(LX_1404)
