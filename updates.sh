6bf2a0c97d3ac612f6bc2c1cee393557708a41aa
MardonHH <markusmenzel@gmail.com>
2016-10-27 22:50:03 +0200

    disable not yet available cherrys

--- updates.sh
+++ updates.sh
@@ -15,38 +15,41 @@ fi
 #Cherrys needed for G3
 
 # rild: support to provide RIL V11 ABI if libril is prebuilt
-cherries+=(CM_164393)
+# cherries+=(CM_164393)
 
 # Support setting RIL's socket names via system property
-cherries+=(CM_164952)
+# cherries+=(CM_164952)
 
 # g3: sepolicy: nougat changes
-cherries+=(CM_161956)
+# cherries+=(CM_161956)
 
 #Cherrys needed for ANZU and G3
 
 # sad selinux 2002
 cherries+=(CM_164156)
 
-#Cherrys needed for ANZU
+# Cherrys needed for ANZU
+
+# libc: use 128-bit cache line size memmove on scorpion
+cherries+=(CM_167386)
 
 # Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(CM_164317)
+cherries+=(CM_167387)
 
 # arm: Allow disabling PIE for dynamically linked executables
-cherries+=(CM_164318)
+cherries+=(CM_167388)
 
 # Allow devices to re-integrate cameraserver and mediaserver
-cherries+=(CM_165008)
+# cherries+=(CM_165008)
 
 # allow cameraserver to have the same UID as mediaserver
-cherries+=(CM_165009)
+# cherries+=(CM_165009)
 
 # CameraService: Allow to use CameraSource as metadata buffer type
-cherries+=(CM_165140)
+# cherries+=(CM_165140)
 
 # temp cam source fix for semc2011
-cherries+=(CM_166099)
+# cherries+=(CM_166099)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
