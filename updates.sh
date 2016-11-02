b56186c11607be49ff3ea21217bc426921f2736c
MardonHH <markusmenzel@gmail.com>
2016-11-02 13:12:48 +0100

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -14,10 +14,10 @@ fi
 
 #Cherries needed for G3
 
-# update wiimote drivers
-cherries+=(CM_168411)
+# diagchar: use diag, not diag_lge
+cherries+=(CM_167836)
 
-# "g3: switch back to thermal-engine"
+# Revert "g3: switch back to thermal-engine"
 cherries+=(CM_168365)
 
 # overlay: launch assist on home long-press
@@ -32,6 +32,9 @@ cherries+=(CM_167624)
 # g3: sepolicy: nougat changes
 cherries+=(CM_167837)
 
+# g3: Rewrite sepolicies
+cherries+=(CM_168197)
+
 # windowmanager: Add support for blur effects
 cherries+=(CM_167370)
 
@@ -43,6 +46,10 @@ cherries+=(CM_167622)
 
 # init: always run baseband-sh
 cherries+=(CM_167787)
+
+# g3: enable legacy camera fixes
+cherries+=(CM_167623)
+
 
 #Cherrys needed for ANZU and G3
 
@@ -61,17 +68,12 @@ cherries+=(CM_167387)
 # arm: Allow disabling PIE for dynamically linked executables
 cherries+=(CM_167388)
 
-# Allow devices to re-integrate cameraserver and mediaserver
-# cherries+=(CM_165008)
+# av: Add support for CameraSource as metadata type
+cherries+=(CM_167485)
 
-# allow cameraserver to have the same UID as mediaserver
-# cherries+=(CM_165009)
+# Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
+cherries+=(CM_167484)
 
-# CameraService: Allow to use CameraSource as metadata buffer type
-# cherries+=(CM_165140)
-
-# temp cam source fix for semc2011
-# cherries+=(CM_166099)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
