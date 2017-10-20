c0d2085b457ebe85057c51db5499b6d49b350c42
Kevin <gsosschneiderle@gmail.com>
2017-10-20 17:48:51 +0200

    Update updates-local.sh

--- updates-local.sh
+++ updates-local.sh
@@ -1,17 +1,32 @@
-# Kernel: add ZEN i/o sheduler
-cherries+=(LX_1470)
+# Add cortex-a73 support
+cherries+=(LAOS_186075)
 
-# New Bootlogo: More material like color
-cherries+=(LX_1471)
+# libc: Add kryo specific memcpy
+cherries+=(LAOS_186051)
 
-# Kernel: OC up to 1,8GHz
-cherries+=(LX_1472)
+# Add kryo support.
+cherries+=(LAOS_185989)
 
-# init: set default interactive max cpu clock @1,4GHz
-cherries+=(LX_1473)
+# libc: ARM: Add 32-bit Kryo memcpy 
+cherries+=(LAOS_185991)
 
-# media_profiles: Enable a working true 720p mode
-cherries+=(LX_1474)
+# libc: use Cortex-A7/A53 memset on Kryo
+cherries+=(LAOS_185990)
 
-# Use Camera HAL1
-cherries+=(LX_1481)
+# Make use of specific Kryo targeting in Clang
+cherries+=(LAOS_ 186006)
+
+# Add arm neon and mips arch features
+cherries+=(LAOS_186074)
+
+# Add support for cortex-a73
+cherries+=(LAOS_186050)
+
+# Add support for an armv7 variant for Kryo 
+cherries+=(LAOS_186003)
+
+# soong: use optimal FPU on Kryo targets
+cherries+=(LAOS_186004)
+
+# Add support for an armv8 variant for Kryo
+cherries+=(LAOS_186005)
