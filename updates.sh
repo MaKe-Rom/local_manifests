3050ec502d9d99eb33bbb6593ccb3d98c8bb8d83
Kevin <gsosschneiderle@gmail.com>
2017-10-20 18:05:29 +0200

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -12,42 +12,6 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
-#Cherries needed for G3
-
-# diagchar: use diag, not diag_lge
-cherries+=(CM_167836)
-
-# overlay: launch assist on home long-press
-cherries+=(CM_167960)
-
-# selinux permissive
-# cherries+=(CM_167624)
-
-# g3: sepolicy: nougat changes
-# cherries+=(CM_167837)
-
-# windowmanager: Add support for blur effects
-cherries+=(CM_167370)
-
-# init: always run baseband-sh
-cherries+=(CM_167787)
-
-#Cherrys needed for ANZU and G3
-
-# sad selinux 2002
-cherries+=(CM_167621)
-
-
-# Cherrys needed for ANZU
-
-# libc: use 128-bit cache line size memmove on scorpion
-cherries+=(CM_167386)
-
-# Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(CM_167387)
-
-# arm: Allow disabling PIE for dynamically linked executables
-cherries+=(CM_167388)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
