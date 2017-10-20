61230269108a63cb73b2efd7a3165493c9eed692
Kevin <gsosschneiderle@gmail.com>
2017-10-20 18:04:59 +0200

    Update updates-local.sh

--- updates-local.sh
+++ updates-local.sh
@@ -7,20 +7,12 @@ cherries+=(LAOS_186051)
 # Add kryo support.
 cherries+=(LAOS_185989)
 
-# libc: ARM: Add 32-bit Kryo memcpy 
-cherries+=(LAOS_185991)
 
 # libc: use Cortex-A7/A53 memset on Kryo
 cherries+=(LAOS_185990)
 
-# Make use of specific Kryo targeting in Clang
-cherries+=(LAOS_ 186006)
-
-# Add arm neon and mips arch features
-cherries+=(LAOS_186074)
-
-# Add support for cortex-a73
-cherries+=(LAOS_186050)
+# libc: ARM: Add 32-bit Kryo memcpy 
+cherries+=(LAOS_185991)
 
 # Add support for an armv7 variant for Kryo 
 cherries+=(LAOS_186003)
@@ -30,3 +22,15 @@ cherries+=(LAOS_186004)
 
 # Add support for an armv8 variant for Kryo
 cherries+=(LAOS_186005)
+
+# Make use of specific Kryo targeting in Clang
+cherries+=(LAOS_ 186006)
+
+# Add support for cortex-a73
+cherries+=(LAOS_186050)
+
+# Add arm neon and mips arch features
+cherries+=(LAOS_186074)
+
+
+
